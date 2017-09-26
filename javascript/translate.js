26c39cbbb11412db04f4c7f207e815bec0873bbe
b-cancel <bryan.o.cancel@gmail.com>
2017-09-25 21:21:19 -0500

    attempt att adding google translate to website... will use yslow to see why its causing the website to be slow when loading

---
+++ javascript/translate.js
@@ -0,0 +1,3 @@
+function googleTranslateElementInit() {
+ 	new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT, gaTrack: true, gaId: 'UA-106844857-1'}, 'google_translate_element');
+}
