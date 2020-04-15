c6598bb1663068db66cdd7252ec286bf68d70316
Bryan Cancel <bryan.o.cancel@gmail.com>
2020-04-15 02:25:48 -0500

    adding new version of site with flutter to add more functionality, staying with the vs code inspired look

---
+++ portfolio/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bryan\'s Portfolio',
+      theme: ThemeData(
+        //dark mode default
+        brightness: Brightness.dark,
+        //none of that ugly green
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("app bar here"),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Text("content and stuff")
+      ),
+    );
+  }
+}
