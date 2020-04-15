ffc13ad438bfb882e9438fe617515d0c530872b7
Bryan Cancel <bryan.o.cancel@gmail.com>
2020-04-15 08:59:39 -0500

    about me section complete after realizing orientation build doesnt work for some reason... moving onto contact section

--- portfolio/lib/bodies/aboutMe.dart
+++ portfolio/lib/bodies/aboutMe.dart
@@ -3,47 +3,98 @@ import 'package:flutter/material.dart';
 class AboutMeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(
-                16.0,
+    return OrientationBuilder(
+      builder: (BuildContext context, Orientation orientation) {
+        if (true) { //orientation == Orientation.portrait
+          print("-------------------------portrait");
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(
+                      16.0,
+                    ),
+                    child: Image.asset(
+                      "assets/face/white.png",
+                      width: 250,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(
+                      16.0,
+                    ),
+                    child: Image.asset(
+                      "assets/title/small.gif",
+                      width: 350,
+                    ),
+                  ),
+                ],
               ),
-              child: Image.asset(
-                "assets/face/white.png",
-                width: 250,
+              Container(
+                child: Flexible(
+                  child: Introduction(),
+                ),
+              )
+            ],
+          );
+        } else {
+          print("-------------------------landscape");
+          return Column(
+            children: <Widget>[
+              Row(
+                children: [
+                  Expanded(
+                    child: Introduction(),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(
+                      16.0,
+                    ),
+                    child: Image.asset(
+                      "assets/face/white.png",
+                      width: 250,
+                    ),
+                  ),
+                ],
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(
-                16.0,
+              Padding(
+                padding: EdgeInsets.all(
+                  16.0,
+                ),
+                child: Image.asset(
+                  "assets/title/large.gif",
+                  width: 350,
+                ),
               ),
-              child: Image.asset(
-                "assets/title/small.gif",
-                width: 350,
-              ),
-            ),
-          ],
-        ),
-        Container(
-          child: Flexible(
-            child: Text(
-              "Hi! I'm Bryan Cancel\n\n"
-              //-------------------------
-              + "I recently graduated with my Bachelors in Computer Science."
-              + " After graduation I traveled Europe, came back and a disaster struck!"
-              + " So I had the privaledge to help in Disaster Releif efforts full time for 3 to 4 months; "
-              + " repairing drywall, roofs, and anything else that needed fixing.\n\n"
-              //-------------------------
-              + "I just finished publishing my first app \"Swol\", and I'm excited for my next Adventure!",
-            ),
-          )
-        )
-      ],
+            ],
+          );
+        }
+      },
+    );
+  }
+}
+
+class Introduction extends StatelessWidget {
+  const Introduction({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "Hi! I'm Bryan Cancel\n\n"
+          //-------------------------
+          +
+          "I recently graduated with my Bachelors in Computer Science." +
+          " After graduation I traveled Europe, came back and a disaster struck!" +
+          " So I had the privaledge to help in Disaster Releif efforts full time for 3 to 4 months; " +
+          " repairing drywall, roofs, and anything else that needed fixing.\n\n"
+          //-------------------------
+          +
+          "I just finished publishing my first app \"Swol\", and I'm excited for my next Adventure!",
     );
   }
 }
