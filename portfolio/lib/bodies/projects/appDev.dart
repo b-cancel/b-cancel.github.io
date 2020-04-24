3b807bbb7fa7532fb7b9160b5747427b654e0db1
Bryan Cancel <bryan.o.cancel@gmail.com>
2020-04-24 11:59:46 -0500

    corrected formatting for the slideshow bit... now to get it to work with listview and actually add images

--- portfolio/lib/bodies/projects/appDev.dart
+++ portfolio/lib/bodies/projects/appDev.dart
@@ -4,6 +4,10 @@ import 'package:flutter/material.dart';
 //internal
 import 'package:portfolio/bodies/projects/projects.dart';
 
+List<String> emptyList(int count){
+  return List.generate(10, (index) => "");
+}
+
 //widget
 class AppDevelopment extends StatelessWidget {
   const AppDevelopment({
@@ -29,6 +33,7 @@ class AppDevelopment extends StatelessWidget {
                   + "so that you are always pushing at your max but never beyond it to avoid injury and maximize results.",
                   github: "https://github.com/b-cancel/swol",
                   googlePlayLink: "https://play.google.com/store/apps/details?id=the.swol.app",
+                  imageUrls: emptyList(10),
                 ),
               ],
             ),
@@ -40,14 +45,17 @@ class AppDevelopment extends StatelessWidget {
               children: <Widget>[
                 ProjectSection(
                   label: "Half Life",
-                  description: "Keep track of the active dosage of medications given their half life,"
-                  + " so you can adjust your dosage until the side effects are worth the desired effects.",
+                  description: "Keep track of the active dosage of medications\n"
+                  + "given their half life, so you can adjust your dosage\n" 
+                  + "until the side effects are worth the desired effects.",
                   github: "https://github.com/b-cancel/halfLife",
+                  imageUrls: emptyList(1),
                 ),
                 ProjectSection(
                   label: "Leashed",
                   description: "Create a digital Leash between your Bluetooth Devices and your phone to avoid loss. Prevention > Recovery.",
                   github: "https://github.com/b-cancel/Leashed_Public",
+                  imageUrls: emptyList(4),
                 ),
               ],
             ),
@@ -63,17 +71,20 @@ class AppDevelopment extends StatelessWidget {
                   + " Handles permission requests for contacts, camera, and photo gallery."
                   " And allows you to quickly jump to a section of contacts with an adaptive scroll bar.",
                   github: "https://github.com/b-cancel/Flutter_ContactPicker",
+                  imageUrls: emptyList(4),
                 ),
                 ProjectSection(
                   label: "Timer & Stopwatch",
                   description: "A re-usable timer and stopwatch with all the functionality you would expect.",
                   github: "https://github.com/b-cancel/Flutter_Timer_And_Stopwatch",
+                  imageUrls: emptyList(1),
                 ),
                 ProjectSection(
                   label: "Material Sheets",
                   description: "Material sheets that can come in from every direction,"
                   + " with the full control, gestures, handles and more.",
                   github: "https://github.com/b-cancel/Flutter_MaterialSheetAndNavigationDrawer",
+                  imageUrls: emptyList(1),
                 ),
               ],
             ),
@@ -87,21 +98,25 @@ class AppDevelopment extends StatelessWidget {
                   label: "Tip Calculator",
                   description: "A Tip Calculator with a very detailed text input formatter",
                   github: "https://github.com/b-cancel/Flutter_CurrencyTextInputFormatter",
+                  imageUrls: emptyList(1),
                 ),
                 ProjectSection(
                   label: "Score Keeper",
                   description: "Keep track of the time and points during a Jiu Jitsu match",
                   github: "https://github.com/b-cancel/Flutter_Cute_Score_Keeper",
+                  imageUrls: emptyList(1),
                 ),
                 ProjectSection(
                   label: "Aero Press Timer Clone",
                   description: "Make the perfect coffee every time",
                   github: "https://github.com/b-cancel/Flutter_AeroPress_Timer_Partial_Clone",
+                  imageUrls: emptyList(1),
                 ),
                 ProjectSection(
                   label: "Instagram Clone",
                   description: "A clone of instagram with flutter",
                   github: "https://github.com/b-cancel/Flutter_Instagram",
+                  imageUrls: emptyList(1),
                 ),
               ],
             ),
--- portfolio/lib/bodies/projects/projects.dart
+++ portfolio/lib/bodies/projects/projects.dart
@@ -13,23 +13,25 @@ import 'package:portfolio/bodies/projects/appDev.dart';
 import 'package:portfolio/bodies/projects/gameDev.dart';
 import 'package:portfolio/bodies/projects/webDev.dart';
 import 'package:portfolio/utils/link/ui/iconLink.dart';
+import 'package:portfolio/utils/link/ui/textLink.dart';
+import 'package:portfolio/utils/wrappedText.dart';
 
 class ProjectsBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(
-        right: 48.0,
+        right: 24.0,
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            "/*\n" 
-            + "I'm updating this section little by little since there is alot of content to edit and display\n"
-            + "When I'm finished I'll take down this message\n"
-            + "But until then feel free to ask for more information on any individual project\n"
-            + "*/",
+            "/*\n" +
+                "I'm updating this section little by little since there is alot of content to edit and display\n" +
+                "When I'm finished I'll take down this message\n" +
+                "But until then feel free to ask for more information on any individual project\n" +
+                "*/",
             style: TextStyle(
               color: Colors.green,
             ),
@@ -59,6 +61,7 @@ class DevelopmentSection extends StatelessWidget {
       labelColor: Colors.white,
       separator: "",
       sectionType: SectionType.Parenthesis,
+      initiallyOpened: false,
       child: child,
     );
   }
@@ -105,76 +108,253 @@ class ProjectSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    BorderSide galleryBorder = BorderSide(
+      color: MyApp.galleryBorder,
+      width: 2,
+    );
+
+    //build
     return CollapsibleSection(
       label: label,
       labelColor: Colors.white,
       separator: "",
       sectionType: SectionType.Parenthesis,
       leftPadding: false,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                "descrip: ",
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 16,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Slideshow(
+              galleryBorder: galleryBorder, 
+              github: github,
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                right: 24,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "descrip: ",
+                        style: GoogleFonts.robotoMono(
+                          color: MyApp.oldGrey,
+                        ),
+                      ),
+                      Flexible(
+                        child: Text(
+                          description + ", ",
+                        ),
+                      ),
+                    ],
+                  ),
+                  Wrap(
+                    children: <Widget>[
+                      Visibility(
+                        visible: livePage != null,
+                        child: IconWebLink(
+                          url: livePage,
+                          label: "Live Page",
+                          icon: IconLinkIcon(
+                            icon: FontAwesome5.window_maximize,
+                          ),
+                        ),
+                      ),
+                      Visibility(
+                        visible: googlePlayLink != null,
+                        child: IconWebLink(
+                          url: googlePlayLink,
+                          label: "Google Play",
+                          icon: IconLinkIcon(
+                            icon: FontAwesome5Brands.google_play,
+                          ),
+                        ),
+                      ),
+                      Visibility(
+                        visible: appStoreLink != null,
+                        child: IconWebLink(
+                          url: appStoreLink,
+                          label: "App Store",
+                          icon: IconLinkIcon(
+                            icon: FontAwesome5Brands.app_store_ios,
+                          ),
+                        ),
+                      ),
+                      Visibility(
+                        visible: github != null && imageUrls == null,
+                        child: IconWebLink(
+                          url: github,
+                          label: "Repository",
+                          icon: IconLinkIcon(
+                            icon: FontAwesome5Brands.github,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Slideshow extends StatelessWidget {
+  const Slideshow({
+    Key key,
+    @required this.galleryBorder,
+    @required this.github,
+  }) : super(key: key);
+
+  final BorderSide galleryBorder;
+  final String github;
+
+  @override
+  Widget build(BuildContext context) {
+    double galleryHeight = 320;
+    return Stack(
+      children: <Widget>[
+        Positioned(
+          top: 0,
+          left: 0,
+          child: Container(
+            height: 0,
+            width: 0,
+            child: OverflowBox(
+              alignment: Alignment.topLeft,
+              minWidth: MediaQuery.of(context).size.width,
+              maxWidth: MediaQuery.of(context).size.width,
+              maxHeight: galleryHeight,
+              minHeight: galleryHeight,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: galleryHeight,
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: galleryBorder,
+                    left: galleryBorder,
+                    bottom: galleryBorder,
+                  ),
+                  color: MyApp.galleryBackground,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    GalleryHeader(
+                      galleryBorder: galleryBorder, 
+                      github: github,
+                    ),
+                    Spacer(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        Container(
+          height: galleryHeight,
+          constraints: BoxConstraints(
+            maxWidth: MediaQuery.of(context).size.width,
+          ),
+          decoration: BoxDecoration(
+            border: Border(
+              top: galleryBorder,
+              left: galleryBorder,
+              bottom: galleryBorder,
+            ),
+            color: MyApp.galleryBackground,
+          ),
+          child: IntrinsicWidth(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                GalleryHeader(
+                  galleryBorder: galleryBorder, 
+                  github: github,
+                ),
+                Spacer(),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class GalleryHeader extends StatelessWidget {
+  const GalleryHeader({
+    Key key,
+    @required this.galleryBorder,
+    @required this.github,
+  }) : super(key: key);
+
+  final BorderSide galleryBorder;
+  final String github;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: MyApp.galleryTopRibbon,
+        border: Border(
+          bottom: galleryBorder,
+        ),
+      ),
+      padding: EdgeInsets.symmetric(
+        horizontal: 4,
+      ),
+      child: Padding(
+        padding: EdgeInsets.only(
+          left: 16,
+          right: 24,
+        ),
+        child: Wrap(
+          crossAxisAlignment: WrapCrossAlignment.center,
+          children: [
+            IconWebLink(
+              url: github,
+              icon: Padding(
+                padding: EdgeInsets.only(
+                  right: 8,
+                ),
+                child: Icon(
+                  FontAwesome.github,
+                ),
+              ),
+            ),
+            TextWebLink(
+              url: github,
+              text: Text(
+                github,
                 style: GoogleFonts.robotoMono(
                   color: MyApp.oldGrey,
                 ),
               ),
-              Expanded(
-                child: Text(description + ", "),
-              ),
-            ],
-          ),
-          Wrap(
-            children: <Widget>[
-              Visibility(
-                visible: livePage != null,
-                child: IconWebLink(
-                  url: livePage, 
-                  label: "Live Page",
-                  icon: IconLinkIcon(
-                    icon: FontAwesome5.window_maximize,
-                  ),
+            ),
+            Text(" - "),
+            TextWebLink(
+              url: github,
+              text: Text(
+                "See More",
+                style: GoogleFonts.robotoMono(
+                  color: Colors.white,
                 ),
               ),
-              Visibility(
-                visible: googlePlayLink != null,
-                child: IconWebLink(
-                  url: googlePlayLink, 
-                  label: "Google Play",
-                  icon: IconLinkIcon(
-                    icon: FontAwesome5Brands.google_play,
-                  ),
-                ),
-              ),
-              Visibility(
-                visible: appStoreLink != null,
-                child: IconWebLink(
-                  url: appStoreLink, 
-                  label: "App Store",
-                  icon: IconLinkIcon(
-                    icon: FontAwesome5Brands.app_store_ios,
-                  ),
-                ),
-              ),
-              Visibility(
-                visible: github != null && imageUrls == null,
-                child: IconWebLink(
-                  url: github,
-                  label: "Repository",
-                  icon: IconLinkIcon(
-                    icon: FontAwesome5Brands.github,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          //TODO: add github gallery link combo
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
