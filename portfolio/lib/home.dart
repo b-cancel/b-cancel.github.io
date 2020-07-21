e0cf14c598f5658c84799c1f03a2a70fd806026b
Bryan Cancel <bryan.o.cancel@gmail.com>
2020-07-20 22:36:04 -0500

    icons setup and looking pretty cute... now moving onto to making the context menus for all except the vcard and qrcode

--- portfolio/lib/home.dart
+++ portfolio/lib/home.dart
@@ -1,13 +1,16 @@
 //flutter
+import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/data.dart';
 import 'package:portfolio/icons/portfolio_icons_icons.dart';
 
 //plugin
 import 'package:dynamic_overflow_menu_bar/dynamic_overflow_menu_bar.dart';
+import 'package:portfolio/utils/conditional.dart';
 import 'package:portfolio/utils/link/ui/hover.dart';
 import 'package:portfolio/utils/link/ui/iconLink.dart';
 import 'package:universal_html/html.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 //internal: other
 import 'package:portfolio/menu/menu.dart';
@@ -46,6 +49,81 @@ class Home extends StatelessWidget {
     );
   }
 
+  OverFlowMenuItem overFlowMenuItem(
+    BuildContext context, {
+    @required String label,
+    @required IconData icon,
+    bool isIrregular: false,
+  }) {
+    return OverFlowMenuItem(
+        onPressed: () {
+          BotToast.showAttachedWidget(
+            attachedBuilder: (_) => Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(
+                  Icons.favorite,
+                  color: Colors.redAccent,
+                ),
+              ),
+            ),
+          );
+        },
+        label: label,
+        child: OpaqueOnHover(
+          invert: false,
+          child: Material(
+            borderRadius: BorderRadius.circular(56),
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: () {
+                printString(
+                  context,
+                  "tap: " + PointerOnHover.isActive().toString(),
+                );
+              },
+              onLongPress: () {
+                printString(
+                  context,
+                  "hold: " + PointerOnHover.isActive().toString(),
+                );
+              },
+              child: Ternary(
+                condition: isIrregular,
+                isTrue: Container(
+                  width: 22.0 + 8 * 2,
+                  height: 22.0 + 8 * 2,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      top: 1,
+                    ),
+                    child: Icon(
+                      icon,
+                      size: 17,
+                    ),
+                  ),
+                ),
+                isFalse: IconLinkIcon(
+                  icon: icon,
+                  mini: true,
+                ),
+              ),
+            ),
+          ),
+        )
+        /*
+          IconWebLink(
+            url: myGithub,
+            lightMode: false,
+            icon: IconLinkIcon(
+              icon: PortfolioIcons.github,
+              mini: true,
+            ),
+            label: "Github",
+          ),*/
+        );
+  }
+
   //build
   @override
   Widget build(BuildContext context) {
@@ -72,13 +150,7 @@ class Home extends StatelessWidget {
                       //VCard should show up for downloading
                       //a shortcut to the QR Code should also show up
 
-                      //TODO: in all cases tapping the label should copy it
-                      //TODO: repair the pop up that isnt showing text for some odd reason
-                      //TODO: if the width is larger than 430 -> use action sheet when pressing contact link
-                      //TODO: the above is particularly important so the behavior is consitent when things start to wrap
-                      OverFlowMenuItem(
-                          onPressed: () {
-                            /*
+                      /*
                           openWithHtml(
           context,
           url,
@@ -118,87 +190,69 @@ class Home extends StatelessWidget {
           ],
         );
                           */
-                          },
-                          label: "Github",
-                          child: OpaqueOnHover(
-                            invert: false,
-                            child: Material(
-                              borderRadius: BorderRadius.circular(56),
-                              color: Colors.transparent,
-                              child: InkWell(
-                                onTap: () {
-                                  printString(
-                                    context,
-                                    "tap: " +
-                                        PointerOnHover.isActive().toString(),
-                                  );
-                                },
-                                onLongPress: () {
-                                  printString(
-                                    context,
-                                    "hold: " +
-                                        PointerOnHover.isActive().toString(),
-                                  );
-                                },
-                                child: IconLinkIcon(
-                                  icon: PortfolioIcons.github,
-                                  mini: true,
-                                ),
-                              ),
-                            ),
-                          )
-                          /*
-                        IconWebLink(
-                          url: myGithub,
-                          lightMode: false,
-                          icon: IconLinkIcon(
-                            icon: PortfolioIcons.github,
-                            mini: true,
-                          ),
-                          label: "Github",
-                        ),*/
-                          ),
-                      /*
-                      OverFlowMenuItem(
-                        onPressed: null,
+
+                      //TODO: in all cases tapping the label should copy it
+                      //TODO: repair the pop up that isnt showing text for some odd reason
+                      //TODO: if the width is larger than 430 -> use action sheet when pressing contact link
+                      //TODO: the above is particularly important so the behavior is consitent when things start to wrap
+
+                      //github link (with specific part highlighted) [also copies link]
+                      //---
+                      //open link (?? icon)***on mobile on tap assume this
+                      //open in new tab (tab icon)
+                      //copy link (copy icon)
+                      overFlowMenuItem(
+                        context,
+                        label: "Github",
+                        icon: PortfolioIcons.github,
+                      ),
+                      //linked in link (with specific part highlighted) [also copies link]
+                      //---
+                      //open link (?? icon)***on mobile on tap assume this
+                      //open in new tab (tab icon)
+                      //copy link (copy icon)
+                      overFlowMenuItem(
+                        context,
                         label: "Linked In",
-                        child: IconWebLink(
-                          url: myLinkedIn,
-                          lightMode: false,
-                          icon: IconLinkIcon(
-                            icon: PortfolioIcons.linkedin,
-                            mini: true,
-                          ),
-                          label: "Linked In",
-                        ),
+                        icon: PortfolioIcons.linkedin,
                       ),
-                      OverFlowMenuItem(
-                        onPressed: null,
+                      //phone number (with specifics highlighted) [also copies number]
+                      //---
+                      //call number
+                      //text number
+                      //copy number ***on mobile on tap assume this
+                      //---
+                      //save V-Card
+                      overFlowMenuItem(
+                        context,
                         label: myNumber,
-                        child: IconPhoneLink(
-                          lightMode: false,
-                          icon: IconLinkIcon(
-                            icon: PortfolioIcons.phone,
-                            mini: true,
-                          ),
-                          url: myNumber,
-                          label: myNumber,
-                        ),
+                        icon: PortfolioIcons.phone,
                       ),
-                      OverFlowMenuItem(
-                        onPressed: null,
+                      //email (with specifics higlighted) [also copies email]
+                      //---
+                      //send email
+                      //copy email ***on mobile on tap assume this
+                      //---
+                      //save V-Card
+                      overFlowMenuItem(
+                        context,
                         label: myEmail,
-                        child: IconEmailLink(
-                          lightMode: false,
-                          icon: IconLinkIcon(
-                            icon: PortfolioIcons.email,
-                            mini: true,
-                          ),
-                          url: myEmail,
-                          label: myEmail,
-                        ),
-                      )
-                      */
+                        icon: PortfolioIcons.email,
+                      ),
+                      //TODO: actually get this to do stuff
+                      overFlowMenuItem(
+                        context,
+                        label: "Contact Card",
+                        icon: PortfolioIcons.address_card,
+                        //icon is irregularly big and improperly centered
+                        isIrregular: true,
+                      ),
+                      //TODO: actually get this to do stuff
+                      overFlowMenuItem(
+                        context,
+                        label: "Scannable Contact Card",
+                        icon: FontAwesomeIcons.qrcode, //TODO: optimize
+                      ),
                     ],
                   ),
                 ),
