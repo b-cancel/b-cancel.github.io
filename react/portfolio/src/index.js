7664d25e00658183acd252d95c0b3b2de1e34bdf
Bryan Cancel <bryan.o.cancel@gmail.com>
2021-10-25 18:57:11 -0400

    swiper configured... now to add my own image before adding the auto height thing

--- react/portfolio/src/index.js
+++ react/portfolio/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Button from "@mui/material/Button";
-import AllProjects from "./projects/project";
+import AllProjects from "./project";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
---
+++ react/portfolio/src/project.js
@@ -0,0 +1,141 @@
+import React, { useRef, useState } from "react";
+
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+// Import Swiper React components
+import {Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+
+// Import Swiper styles
+import "./styles.css";
+
+// import Swiper core and required modules
+import SwiperCore, { Lazy, Pagination, Navigation } from "swiper";
+
+// install Swiper modules
+SwiperCore.use([Lazy, Pagination, Navigation]);
+
+export default function AllProjects() {
+  const projects = [
+    {
+      name: "Get Swol App",
+      description:
+        "Swol helps you use One Rep Max Equations to reach your Weightlifting Goals Faster." +
+        "Know exactly how much weight you should be lifting given your training type and your previous set," +
+        "so that you are always pushing at your max but never beyond it to avoid injury and maximize results.",
+      github: "https://github.com/b-cancel/swol",
+      appstore: "https://apps.apple.com/us/app/get-swol-app/id1576936894",
+      googleplay: "https://play.google.com/store/apps/details?id=get.swol.app",
+      gallery: [
+        "https://i.imgur.com/SLpWBkv",
+        "https://i.imgur.com/nLhuzZR",
+        "https://i.imgur.com/eiERhkP",
+        "https://i.imgur.com/UgtbjK8",
+        "https://i.imgur.com/cqPSXbg",
+        "https://i.imgur.com/yne1JDc",
+        "https://i.imgur.com/gkcEJHH",
+      ],
+    },
+    {
+      name: "Service Helper",
+      description: "write a breif description of service helper",
+      appstore: "https://testflight.apple.com/join/ZFowXCT4",
+      gallery: [
+        "https://i.imgur.com/tARG4m0",
+        "https://i.imgur.com/ST8VhbZ",
+        "https://i.imgur.com/mqQYv0v",
+        "https://i.imgur.com/oPfmGVV",
+        "https://i.imgur.com/tfAz8lp",
+        "https://i.imgur.com/0IHtYld",
+        "https://i.imgur.com/edfqUaa",
+      ],
+    },
+  ];
+
+  function ProjectCard({ project }) {
+    return (
+      <Card sx={{ minWidth: 275, margin: 1 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            {project.name}
+          </Typography>
+          <Typography variant="h5" component="div">
+            aaa
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            ddd
+          </Typography>
+          <Typography variant="body2">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+
+        <Swiper
+          navigation={true}
+          slidesPerView={"auto"}
+          grabCursor={true}
+          loop={true}
+          centeredSlides={false}
+          spaceBetween={30}
+          pagination={{
+            type: "progressbar",
+          }}
+          style={{
+            "--swiper-navigation-color": "#fff",
+            "--swiper-pagination-color": "#fff",
+          }}
+          lazy={true}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img
+              data-src="https://swiperjs.com/demos/images/nature-1.jpg"
+              className="swiper-lazy"
+            />
+            <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          </SwiperSlide>
+        </Swiper>
+
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    );
+  }
+
+  return projects.map((project) => (
+    <ProjectCard project={project}></ProjectCard>
+  ));
+}
