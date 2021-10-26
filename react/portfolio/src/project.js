27b046a6f2874f984303d7eb51566be4d9953559
Bryan Cancel <bryan.o.cancel@gmail.com>
2021-10-26 16:44:50 -0400

    refined project descriptions... adding the options to add descriptions to media before adding the ability to show GIFS

--- react/portfolio/src/project.js
+++ react/portfolio/src/project.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Text } from "react-native";
 
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
@@ -11,6 +12,11 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import ProgressiveImage from "react-progressive-graceful-image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Stack from "@mui/material/Stack";
+
+import { Markup } from 'react-render-markup';
+import { renderMarkup } from 'react-render-markup';
+
 
 import {
   faGithub,
@@ -42,11 +48,42 @@ SwiperCore.use([Mousewheel, Keyboard, Lazy, Pagination, Navigation]);
 export default function AllProjects() {
   const projects = [
     {
+      name: "Service Helper",
+      description: 
+      `A Digital Version of the "House To House Record" created to solve all the problems that arise when using pen and paper like<br>
+      [1] Forgetting a pen and/or paper<br>
+      [2] Forgetting "Who's In Charge" of the territory and not knowing who to send the record to<br>
+      [3] Forgetting to write down the "Territory Number" and not knowing what territory the records belong to<br>
+      [4] Not being able to locate the address of a revisit because your wrote "St" instead of "Ct"<br>
+      [5] Finding someone that is interested but speaks another language but not having a "Follow Up" Form<br>
+      [7] Forgetting to write down the address of the previous house and having to walk back to get it
+      [6] and more!<br>
+      `,
+      appstore: "https://testflight.apple.com/join/ZFowXCT4",
+      gallery: [
+        "https://i.imgur.com/tARG4m0.jpg",
+        "https://i.imgur.com/ST8VhbZ.jpg",
+        "https://i.imgur.com/mqQYv0v.jpg",
+        "https://i.imgur.com/oPfmGVV.jpg",
+        "https://i.imgur.com/tfAz8lp.jpg",
+        "https://i.imgur.com/0IHtYld.jpg",
+        "https://i.imgur.com/edfqUaa.jpg",
+      ],
+    },
+    {
       name: "Get Swol App",
-      description:
-        "Swol helps you use One Rep Max Equations to reach your Weightlifting Goals Faster." +
-        "Know exactly how much weight you should be lifting given your training type and your previous set," +
-        "so that you are always pushing at your max but never beyond it to avoid injury and maximize results.",
+      description: 
+      `One Rep Max equations can estimate your one rep max given any other set.<br>
+      The "Get Swol App" re-purposes these equations, and uses an average of the 7 equations out there to<br>
+      "calculate the max ammount of weight you sould be able to lift given your desired rep target"<br>
+      <br>
+      By knowing your upper limit you will be able to<br>
+      [a] plan your workout even better [b] reach your weight lifting goals faster [c] and avoid injury<br>
+      <br>
+      Additionally, it will help you<br>
+      [1] do the right amount of reps [2] take appropriate break between your sets [3] and do the right amount of sets<br>
+      for your selected training style<br>
+      `,
       github: "https://github.com/b-cancel/swol",
       appstore: "https://apps.apple.com/us/app/get-swol-app/id1576936894",
       googleplay: "https://play.google.com/store/apps/details?id=get.swol.app",
@@ -58,20 +95,6 @@ export default function AllProjects() {
         "https://i.imgur.com/cqPSXbg.jpg",
         "https://i.imgur.com/yne1JDc.jpg",
         "https://i.imgur.com/gkcEJHH.jpg",
-      ],
-    },
-    {
-      name: "Service Helper",
-      description: "write a breif description of service helper",
-      appstore: "https://testflight.apple.com/join/ZFowXCT4",
-      gallery: [
-        "https://i.imgur.com/tARG4m0.jpg",
-        "https://i.imgur.com/ST8VhbZ.jpg",
-        "https://i.imgur.com/mqQYv0v.jpg",
-        "https://i.imgur.com/oPfmGVV.jpg",
-        "https://i.imgur.com/tfAz8lp.jpg",
-        "https://i.imgur.com/0IHtYld.jpg",
-        "https://i.imgur.com/edfqUaa.jpg",
       ],
     },
   ];
@@ -129,6 +152,27 @@ export default function AllProjects() {
       />
     ) : null;
 
+    function NewlineText(props) {
+      const text = props.text;
+      const newText = text.split('\n').map(str => {return (<span>{str}</span>)});
+      return (<Stack>{newText}</Stack>);
+    }
+    /*
+    <style
+              dangerouslySetInnerHTML={{
+                __html: `
+                .swiper {
+                  height: ${imageHeight}px;
+                }
+                
+                .swiper-slide {
+                  height: ${imageHeight}px;
+                }
+              `,
+              }}
+            />
+    */
+    
     return (
       <Card sx={{ margin: "16px", mb: "36px" }}>
         <CardContent>
@@ -149,19 +193,7 @@ export default function AllProjects() {
         </CardContent>
         <Box sx={{ backgroundColor: "#1e1e1e" }}>
           <Box>
-            <style
-              dangerouslySetInnerHTML={{
-                __html: `
-                .swiper {
-                  height: ${imageHeight}px;
-                }
-                
-                .swiper-slide {
-                  height: ${imageHeight}px;
-                }
-              `,
-              }}
-            />
+            
             <Swiper
               navigation={true}
               slidesPerView={slidesPerView}
@@ -182,13 +214,13 @@ export default function AllProjects() {
             >
               {project.gallery &&
                 project.gallery.map((media) => (
-                  <SwiperSlide key={media} height={imageHeight}>
+                  <SwiperSlide key={media}>
                     <ProgressiveImage src={media} placeholder="">
                       {(src, loading) => {
                         return loading ? (
                           <Box
                             sx={{
-                              height: "100%",
+                              height:imageHeight,
                               width: "90%",
                               bgcolor: "#000",
                               borderRadius: "8px",
@@ -206,7 +238,7 @@ export default function AllProjects() {
                             </Box>
                           </Box>
                         ) : (
-                          <img src={src} alt={src} />
+                          <img src={src} alt={src} height={imageHeight}/>
                         );
                       }}
                     </ProgressiveImage>
@@ -216,7 +248,10 @@ export default function AllProjects() {
           </Box>
         </Box>
         <CardContent>
-          <Typography variant="body2">{project.description}</Typography>
+          
+          <Typography variant="body2">
+          {renderMarkup(project.description)}
+          </Typography>
         </CardContent>
       </Card>
     );
