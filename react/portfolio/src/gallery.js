72ddd1c9c0eae059cff81149c99d6b8ab31b3b8b
Bryan Cancel <bryan.o.cancel@gmail.com>
2021-10-27 19:42:21 -0400

    basic implementation of the side menu that DOES NOT open automatically

---
+++ react/portfolio/src/gallery.js
@@ -0,0 +1,71 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AllProjects from "./project";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import QuickLinks from "./contact.js";
+
+export default function Gallery() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
+  return (
+    <SafeAreaProvider>
+      <ThemeProvider theme={darkTheme}>
+        <Box sx={{ mt: "56px" }}>
+          <Stack>
+            <AllProjects />
+            <Stack sx={{ mb: "56px" }}>
+              <QuickLinks />
+              <Typography
+                variant="subtitle2"
+                color="white"
+                sx={{ textAlign: "center" }}
+              >
+                All Right Reserved
+              </Typography>
+              <Typography
+                variant="subtitle2"
+                color="white"
+                sx={{ textAlign: "center" }}
+              >
+                @ 2021 Bryan Cancel
+              </Typography>
+            </Stack>
+          </Stack>
+        </Box>
+        <Box
+          sx={{
+            position: "fixed",
+            zIndex: 99,
+            width: "100%",
+            textAlign: "center",
+          }}
+        >
+          <Box
+            sx={{
+              display: "inline-block",
+              backgroundColor: "#fff",
+              borderRadius: "8px",
+              px: "8px",
+              py: "4px",
+              my: "8px",
+            }}
+          >
+            <Stack>
+              <Typography variant="subtitle2" sx={{ fontWeight: "bold" }}>
+                DEV, UX & UI by Bryan Cancel
+              </Typography>
+            </Stack>
+          </Box>
+        </Box>
+      </ThemeProvider>
+    </SafeAreaProvider>
+  );
+}
--- react/portfolio/src/index.js
+++ react/portfolio/src/index.js
@@ -1,79 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import AllProjects from "./project";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
-import QuickLinks from "./contact.js";
+
+import "./styles.css";
+import SideBar from "./sidebar";
+
+import Gallery from "./gallery.js"
 
 function App() {
-  const darkTheme = createTheme({
-    palette: {
-      mode: "dark",
-    },
-  });
-
-  /*
-  <Box sx={{margin:"auto", mt:"16px"}}>
-    <img src="./miniLoader.gif" alt="loading" height="56px" width="56px"/>
-  </Box>
-  */
-
   return (
-    <SafeAreaProvider>
-      <ThemeProvider theme={darkTheme}>
-        <Box sx={{ mt: "56px" }}>
-          <Stack>
-            <AllProjects />
-            <Stack sx={{ mb: "56px" }}>
-              <QuickLinks />
-              <Typography
-                variant="subtitle2"
-                color="white"
-                sx={{ textAlign: "center" }}
-              >
-                All Right Reserved
-              </Typography>
-              <Typography
-                variant="subtitle2"
-                color="white"
-                sx={{ textAlign: "center" }}
-              >
-                @ 2021 Bryan Cancel
-              </Typography>
-            </Stack>
-          </Stack>
-        </Box>
-        <Box
-          sx={{
-            position: "fixed",
-            zIndex: 99,
-            width: "100%",
-            textAlign: "center",
-          }}
-        >
-          <Box
-            sx={{
-              display: "inline-block",
-              backgroundColor: "#fff",
-              borderRadius: "8px",
-              px: "8px",
-              py: "4px",
-              my: "8px",
-            }}
-          >
-            <Stack>
-              <Typography variant="subtitle2" sx={{ fontWeight: "bold" }}>
-                DEV, UX & UI by Bryan Cancel
-              </Typography>
-            </Stack>
-          </Box>
-        </Box>
-      </ThemeProvider>
-    </SafeAreaProvider>
+    <div id="App">
+      <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
+      <div id="page-wrap">
+        <Gallery/>
+      </div>
+    </div>
   );
 }
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
