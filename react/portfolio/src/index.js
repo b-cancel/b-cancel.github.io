73a9afe7c545746df398865509babf47f8327099
Bryan Cancel <bryan.o.cancel@gmail.com>
2021-10-27 21:54:59 -0400

    sync animations between menu button and side bar... tomorrow i'll get this to work and then work on scaling the bar properly before stuffing it with content

--- react/portfolio/src/index.js
+++ react/portfolio/src/index.js
@@ -6,6 +6,21 @@ import SideBar from "./sidebar";
 import Gallery from "./gallery.js"
 
 function App() {
+  //<Menu isOpen={ true } />
+  //<Menu width={ '280px' } />
+  //<Menu onClose={ handleOnClose } />
+  //<Menu onOpen={ handleOnOpen } />
+
+  //TODO: trigger the variable change... then use 
+  /*
+  Note: If you want to render the menu open initially, 
+  you will need to set this property in your parent component's componentDidMount() function.
+  https://github.com/negomi/react-burger-menu/wiki/FAQ#i-want-to-control-the-open-state-programmatically-but-i-dont-understand-how-to-use-the-isopen-prop
+  */
+  function onMenuOpen(){
+    window.onMenuOpened();
+  }
+
   return (
     <div id="outer-container">
       <SideBar pageWrapId={"page-wrap"}/>
